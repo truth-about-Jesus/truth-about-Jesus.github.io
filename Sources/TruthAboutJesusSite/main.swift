a7540dd706d854f982605046f2351bf5eb149453
Enid Ning <enidn@me.com>
2023-07-02 11:55:10 -0400

    Force refresh of styles.css by client browsers (overrode static func head<T: Website>(...) in Node in Publish API PlotComponents.swift to add a ?rnd=132 to stylesheet link in html header). Comments on extensions in main.swift.

--- Sources/TruthAboutJesusSite/main.swift
+++ Sources/TruthAboutJesusSite/main.swift
@@ -27,7 +27,11 @@ struct TruthAboutJesusSite: Website {
     var imagePath: Path? { nil }
 }
 
+// This will generate your website using the built-in Foundation theme:
+try TruthAboutJesusSite().publish(withTheme: .truthAboutJesusTheme)
 
+
+// helps with breaking up strings containing translation (or verse and reference) with 2 spaces between the 2 parts
 extension String {
 
     var doubleSpaceArray: [String] {
@@ -43,6 +47,51 @@ extension String {
     }
 }
 
+// changed stylesheetPath to include ?rnd=132 so that stylesheet will be refreshed by client browsers, not just use old cached stylesheet
+extension Node where Context == HTML.DocumentContext {
+    static func head<T: Website>(
+        for location: Location,
+        on site: T,
+        titleSeparator: String = " | ",
+        stylesheetPaths: [Path] = ["/styles.css?rnd=132"],
+        rssFeedPath: Path? = .defaultForRSSFeed,
+        rssFeedTitle: String? = nil
+    ) -> Node {
+        var title = location.title
 
-// This will generate your website using the built-in Foundation theme:
-try TruthAboutJesusSite().publish(withTheme: .truthAboutJesusTheme)
+        if title.isEmpty {
+            title = site.name
+        } else {
+            title.append(titleSeparator + site.name)
+        }
+
+        var description = location.description
+
+        if description.isEmpty {
+            description = site.description
+        }
+
+        return .head(
+            .encoding(.utf8),
+            .siteName(site.name),
+            .url(site.url(for: location)),
+            .title(title),
+            .description(description),
+            .twitterCardType(location.imagePath == nil ? .summary : .summaryLargeImage),
+            .forEach(stylesheetPaths, { .stylesheet($0) }),
+            .viewport(.accordingToDevice),
+            .unwrap(site.favicon, { .favicon($0) }),
+            .unwrap(rssFeedPath, { path in
+                let title = rssFeedTitle ?? "Subscribe to \(site.name)"
+                return .rssFeedLink(path.absoluteString, title: title)
+            }),
+            .unwrap(location.imagePath ?? site.imagePath, { path in
+                let url = site.url(for: path)
+                return .socialImageLink(url)
+            })
+        )
+    }
+
+}
+
+
