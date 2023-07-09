e0f6e238bee8f64ff16171c768ea34b4f720602f
Enid Ning <enidn@me.com>
2023-07-09 17:34:59 -0400

    Removed matomo folder from Resources. Only in docs now.

--- Resources/matomo/plugins/SitesManager/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-    'SitesManager.SiteWithoutDataStartTrackingTranslation' => DI\string('SitesManager_SiteWithoutDataStartTrackingDataDescriptionLine1'),
-    'SitesManager.SiteWithoutDataVueFollowStepNote2' => DI\string('SitesManager_SiteWithoutDataVueFollowStepNote2')
-);
