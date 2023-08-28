225892d3cedc63ca3e6193f6e7afb881e6fb5f22
Enid Ning <enidn@me.com>
2023-08-28 08:50:57 -0400

    Deleted Chinese fonts from styles.css. New styles.css version 22.

--- Sources/TruthAboutJesusSite/main.swift
+++ Sources/TruthAboutJesusSite/main.swift
@@ -54,7 +54,7 @@ extension Node where Context == HTML.DocumentContext {
         on site: T,
         titleSeparator: String = " | ",
         // changed stylesheetPath to include ?version=0 so that stylesheet will be refreshed by client browsers, not just use old cached stylesheet
-        stylesheetPaths: [Path] = ["/styles.css?version=21"],
+        stylesheetPaths: [Path] = ["/styles.css?version=22"],
 //        stylesheetPaths: [Path] = ["/styles.css"],
         rssFeedPath: Path? = .defaultForRSSFeed,
         rssFeedTitle: String? = nil
