c4519956044514868c8e2a51d45fefda6f7adae2
eyrie <33135351+truth-about-Jesus@users.noreply.github.com>
2025-06-06 00:57:50 -0400

    Remove credit+link in footer in Theme+TruthAboutJesus.swift

--- Sources/TruthAboutJesusSite/Theme+TruthAboutJesus.swift
+++ Sources/TruthAboutJesusSite/Theme+TruthAboutJesus.swift
@@ -291,9 +291,8 @@ private struct SiteFooter: Component {
                 Link("RSS feed 提要", url: "/feed.rss")
             }
             Paragraph {
-                Text("© 2024-2025 ")
-                Link("Enid Ning", url: "mailto: truth-about-jesus@proton.me")
-                Text(", in collaboration with Jesus of Nazareth, Son of God, Son of Man, Creator, Sustainer and Ruler of the Universe.")
+                Text("© ")
+                Text("Jesus of Nazareth, Son of God, Son of Man, Creator, Sustainer and Ruler of the Universe.")
             }
             .style("margin-bottom: 40px;")
         }
